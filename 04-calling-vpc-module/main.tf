14892b96d736dd3448506a096402132ca14b4397
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-06-24 10:58:27 +0530

    terraform

--- 04-calling-vpc-module/main.tf
+++ 04-calling-vpc-module/main.tf
@@ -1,6 +1,6 @@
 module "VPC" {
  # source       = "../03-VPC-module"
-  source       = "git:://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=main"
+  source       = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=main"
   vpc_cidr     = var.vpc_cidr
   environment  = var.environment
   project_name = var.project_name
